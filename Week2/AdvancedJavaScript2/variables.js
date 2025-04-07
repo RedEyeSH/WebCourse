dd0fc1a0ee2f13a48d1dd48646da4ba0374292a6
RedEyeSH <97534002+RedEyeSH@users.noreply.github.com>
2025-04-07 11:19:27 +0300

    First component of react

--- Week2/AdvancedJavaScript2/variables.js
+++
@@ -1,1 +0,0 @@
-export const apiUrl = 'https://media2.edu.metropolia.fi/restaurant/api/v1';
--- Week2/JS-recap5/fetchData.js
+++
@@ -1,8 +0,0 @@
-async function fetchData(url, options) {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-        throw new Error(`${response.statusText} ${response.status}`);
-    }
-    const jsonData = await response.json();
-    return jsonData;
-}
